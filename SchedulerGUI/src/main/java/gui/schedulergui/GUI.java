11e16efd72082257825ea3a49bb224350e2f1a47
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-07-07 23:04:52 +0200

    Add files via upload

--- SchedulerGUI/src/main/java/gui/schedulergui/GUI.java
+++ SchedulerGUI/src/main/java/gui/schedulergui/GUI.java
@@ -1,37 +1,46 @@
 package gui.schedulergui;
 
+import gui.schedulergui.controllers.CompartmentEditDialogController;
+import gui.schedulergui.controllers.CompartmentWrapper;
+import gui.schedulergui.controllers.CompletedTasksDialog;
+import gui.schedulergui.controllers.Controller;
+import gui.schedulergui.utilities.Task;
+import gui.schedulergui.utilities.TaskEditDialogController;
+import gui.schedulergui.utilities.TaskWrapper;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import loader.CompartmentLoader;
-import loader.TaskLoader;
-import utilites.Compartment;
-import utilites.Task;
 
+import javax.xml.bind.*;
+import java.io.File;
 import java.io.IOException;
 
 public class GUI extends Application {
 
     private ObservableList<Task> tasks = FXCollections.observableArrayList();
-    private ObservableList<Compartment> compartments = FXCollections.observableArrayList();
+    private ObservableList<String> compartments = FXCollections.observableArrayList();
+    private ObservableList<Task> completedTasks = FXCollections.observableArrayList();
 
     public GUI() {
-        tasks = TaskLoader.loadTasks();
-        compartments = CompartmentLoader.loadCompartments();
+        loadTasks();
+        loadCompartments();
+        loadCompletedTasks();
     }
 
-    private Stage primaryStage;
+    public Stage primaryStage;
 
     @Override
     public void start(Stage stage) throws IOException {
         this.primaryStage = stage;
         this.primaryStage.setTitle("SchedulerGUI");
+        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
         showTasksOverview();
     }
@@ -48,6 +57,7 @@ public class GUI extends Application {
 
             Controller controller = loader.getController();
             controller.setGUI(this);
+            controller.setCompletedTasks(completedTasks);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -111,7 +121,7 @@ public class GUI extends Application {
             dialogStage.showAndWait();
 
             if (controller.isOkInput()) {
-                Compartment compartment = controller.getCompartment();
+                String compartment = controller.getCompartment();
                 if (!compartments.contains(compartment))
                     compartments.add(compartment);
             }
@@ -121,12 +131,146 @@ public class GUI extends Application {
         }
     }
 
+    public void showCompletedTasksDialog(ObservableList<Task> completedTasks){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("CompletedTasksTableView.fxml"));
+            AnchorPane tasks = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Completed tasks");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(tasks);
+            dialogStage.setScene(scene);
+
+            CompletedTasksDialog controller = loader.getController();
+            controller.showCompletedTasks(completedTasks);
+
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public ObservableList<Task> getTasks() {
         return tasks;
     }
 
-    public ObservableList<Compartment> getCompartments() {
+    public ObservableList<String> getCompartments() {
         return compartments;
+    }
+
+    public void saveTasks(ObservableList<Task> itemsToSave){
+        try {
+            String filePath = System.getProperty("user.dir") + "/src/main/resources/gui/schedulergui/saves/taskSaves.xml";
+            File file = new File(filePath);
+            JAXBContext context = JAXBContext.newInstance(TaskWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            //Wrapping to XML file
+            TaskWrapper wrapper = new TaskWrapper();
+            wrapper.setTasks(itemsToSave);
+
+            m.marshal(wrapper, file);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadTasks(){
+        try{
+            String filePath = System.getProperty("user.dir") + "/src/main/resources/gui/schedulergui/saves/taskSaves.xml";
+            File file = new File(filePath);
+            JAXBContext context = JAXBContext.newInstance(TaskWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            if (file.length() != 0) {
+                TaskWrapper wrapper = (TaskWrapper) um.unmarshal(file);
+
+                if (wrapper.getTasks() != null)
+                    tasks.addAll(wrapper.getTasks());
+            }
+
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadCompartments(){
+        try{
+            String filePath = System.getProperty("user.dir") + "/src/main/resources/gui/schedulergui/saves/compartmentSaves.xml";
+            File file = new File(filePath);
+            JAXBContext context = JAXBContext.newInstance(CompartmentWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            if (file.length() != 0) {
+                CompartmentWrapper wrapper = (CompartmentWrapper) um.unmarshal(file);
+
+                if (wrapper.getCompartments() != null)
+                    compartments.addAll(wrapper.getCompartments());
+            }
+
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveCompartments(){
+        try{
+            String filePath = System.getProperty("user.dir") + "/src/main/resources/gui/schedulergui/saves/compartmentSaves.xml";
+            File file = new File(filePath);
+            JAXBContext context = JAXBContext.newInstance(CompartmentWrapper.class);
+            Marshaller m = context.createMarshaller();
+
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            //Wrapping to XML file
+            CompartmentWrapper wrapper = new CompartmentWrapper();
+            wrapper.setCompartments(compartments);
+
+            m.marshal(wrapper, file);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveCompletedTasks(){
+        try{
+            String filePath = System.getProperty("user.dir") + "/src/main/resources/gui/schedulergui/saves/completedTasksSave.xml";
+            File file = new File(filePath);
+            JAXBContext context = JAXBContext.newInstance(TaskWrapper.class);
+            Marshaller m = context.createMarshaller();
+
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            TaskWrapper wrapper = new TaskWrapper();
+            wrapper.setTasks(completedTasks);
+
+            m.marshal(wrapper, file);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadCompletedTasks(){
+        try{
+            String filePath = System.getProperty("user.dir") + "/src/main/resources/gui/schedulergui/saves/completedTasksSave.xml";
+            File file = new File(filePath);
+            JAXBContext context = JAXBContext.newInstance(TaskWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            if (file.length() != 0) {
+                TaskWrapper wrapper = (TaskWrapper) um.unmarshal(file);
+
+                if (wrapper.getTasks() != null)
+                    completedTasks.addAll(wrapper.getTasks());
+            }
+
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
--- SchedulerGUI/src/main/java/module-info.java
+++ SchedulerGUI/src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module gui.schedulergui {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
-    opens gui.schedulergui to javafx.fxml;
+    requires java.xml.bind;
+
+    opens gui.schedulergui;
+
     exports gui.schedulergui;
+    exports gui.schedulergui.utilities;
+    opens gui.schedulergui.utilities;
+    exports gui.schedulergui.controllers;
+    opens gui.schedulergui.controllers;
 }
+
+
