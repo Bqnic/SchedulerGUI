c893c879384899cdb81d5aa493154316ed28b78b
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-07-10 10:46:57 +0200

    Add files via upload

--- src/main/java/gui/schedulergui/Main.java
+++ src/main/java/gui/schedulergui/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args){
+        System.setProperty("prism.forceGPU", "true");
         Application.launch(GUI.class);
     }
 }
